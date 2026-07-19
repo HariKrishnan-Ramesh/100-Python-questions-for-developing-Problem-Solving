8ca3811f73426c796ca6e459feb9a3835d7d20b9
Harikrishna Ramesh <harikrishnaramesh777@gmail.com>
2026-07-19 20:59:47 +0530

    products of a digit

--- Codes/prgrm36.py
+++ Codes/prgrm36.py
@@ -1,9 +1,9 @@
 #Write a program to find the product of all digits of a number n.
-def sum_digits(n):
+def product_digits(n):
   prod = 1
   for i in n:
     prod *= int(i)
   print(prod)
 
 num = input("Enter the number: ")
-sum_digits(num)
+product_digits(num)
